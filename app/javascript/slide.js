840e64d1c0d9603fa7e8a4277a6f557dbffac42f
misato-yabe <mumumutt58@gmail.com>
2020-12-18 23:18:33 +0900

    slick,font-awesomeの導入

---
+++ app/javascript/slide.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.slider').slick({
+    speed: 1000,
+    dots: true,
+    prevArrow: false,
+    nextArrow: false
+
+    // prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    // nextArrow: '<button class="slide-arrow next-arrow"></button>',
+    // dotsClass: 'slide-dots'
+  });
+});
+
