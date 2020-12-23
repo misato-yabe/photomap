e7072d854e6cd227c745149b24405a08fd226b1c
misato-yabe <mumumutt58@gmail.com>
2020-12-23 15:30:39 +0900

    user編集ページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!,only:[:edit,:new]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def show
     @user = User.find(params[:id])
     @photos = @user.photos.order("created_at DESC")
-
   end
 
   def edit
-    
+    @user = User.find(params[:id])
+    unless user_signed_in? && current_user.id == @user.user_id
+      redirect_to action: :index
+    end
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:image, :consept])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2020_12_11_065558) do
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
+    t.text "concept"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
