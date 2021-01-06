31c7aaedf1a0698db68c74b5cbb70c21b6015a98
misato-yabe <mumumutt58@gmail.com>
2021-01-06 15:42:53 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     devise_for :users, controllers: {
       sessions: 'users/sessions'
+      omniauth_callbacks: 'users/omniauth_callbacks',
+      registrations: 'users/registrations'
     }
 
   root 'toppages#index'
